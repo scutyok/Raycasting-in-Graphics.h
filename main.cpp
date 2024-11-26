88aa8244fc22a03debf0a34a4f7bf1156580d49c
scutyok <scutynicolaiciuc@gmail.com>
2024-11-26 17:18:28 +0200

    update again?

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ double posX, posY;
 int mapData(char n[])
 {
 	char s[100] = {'.', '/', 'm', 'a', 'p', 's', '/', 'm', 'a', 'p',n[0],n[1],n[2],n[3], '.', 'i', 'n'};
-    cout << s;
     ifstream fin(s);
     int i=1, j=1, jnk;
     char ch,oldch=1;
@@ -316,7 +315,7 @@ int main()
                 bar(x, drawStart, x + 1, drawEnd);
             }
 
-
+            swapbuffers();
             oldTime = time;
             time++;
 
